2f86746935a145357df694622a3eb39800be07ca
Thiago <prattithiago5@gmail.com>
2023-08-31 15:44:05 -0300

    ADD Fonts

--- src/pages/style.js
+++ src/pages/style.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const Teste = styled.div`
-   background: red;
    
    
 `
--- src/styles/global.js
+++ src/styles/global.js
@@ -9,7 +9,30 @@ export default createGlobalStyle`
 
     body{
         background-color:${({theme}) => theme.COLORS.BACKGROUND_800};
+        color: ${({theme}) => theme.COLORS.WHITE};
+        -webkit-font-smoothing: antialiased;
     }
+
+    a{
+        text-align: none;
+    }
+
+    button, a{
+        cursor: pointer;
+        transition: filter 0.2s;
+    }
+
+    body, input, button, textarea{
+        font-family: 'Roboto Slab', serif;
+        font-size: 16px;
+        outline: none;
+    }
+
+    button:hover, a:hover{
+        filter: brightness(0.9);
+        
+    }
+
 
 
 
