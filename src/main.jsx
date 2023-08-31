eefd341477c31ed48dc6bb77ee6102639bfef500
Thiago <prattithiago5@gmail.com>
2023-08-31 15:14:57 -0300

    ADD Styles Pack

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './pages/App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/global';
+import { App } from './pages/App';
+import theme  from './styles/theme';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider theme={theme}>  
+    <GlobalStyles />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 )
--- src/pages/style.js
+++ src/pages/style.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
 export const Teste = styled.div`
+   background: red;
+   
    
 `
