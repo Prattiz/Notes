b785782307e1206e7a09d03311e6616e7105a9f7
Thiago <prattithiago5@gmail.com>
2023-09-01 15:06:39 -0300

    UPDATE Header CSS

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,15 @@
-import { Conteiner } from "./styles"
+import { Conteiner, Profile } from "./styles"
 
 export function Header(){
     return(
         <Conteiner>
+            <Profile>
+                <img src="https://github.com/Prattiz.png" alt="Foto do Usuário" />
+                <div>
+                    <span>Bem Vindo, </span>
+                    <strong>Thiago</strong>
+                </div>
+            </Profile>
             
         </Conteiner>
     )
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -8,7 +8,38 @@ export const Conteiner = styled.header`
     border-bottom:1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700};
     display: flex;
     justify-content: space-between;
-    background-color: red;
+    padding:0 80px;
 
+
+`
+
+export const Profile = styled.div`
+
+    display: flex;
+    gap: 16px;
+    align-items: center;
+
+    >img{
+        width: 70px;
+        height:70px;
+        border-radius: 50%;
+    }
+
+    >div{
+        display: flex;
+        flex-direction: column;
+        line-height: 24px;
+
+
+        span{
+            font-size: 14px;
+            color:${({ theme }) => theme.COLORS.GRAY_100};
+        }
+
+        strong{
+            font-size: 18px;
+            color:${({ theme }) => theme.COLORS.WHITE};
+        }
+    }
 
 `
