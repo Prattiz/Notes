158bbf2469d9c9f67027fca5d786269408a67468
Thiago <prattithiago5@gmail.com>
2023-09-06 15:08:44 -0300

    SIGN IN AND SIGN UP FISNISHED

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,16 +1,22 @@
 import { Background, Container, Form } from "./style";
 import { Input } from "../../components/Input";
-import { FiMail, FiLock} from "react-icons/fi";
+import { FiMail, FiUser, FiLock} from "react-icons/fi";
 import { Button } from "../../components/Buttons"
 
-export function SignIn(){
+export function SignUp(){
     return(
         <Container>
+            <Background />
             <Form>
                 <h1>RocketNotes</h1>
                 <p>Aplicação para salvar e gerenciar seus Links úteis</p>
-                <h2>Faça seu Login</h2>
+                <h2>Crie Sua Conta</h2>
 
+                <Input 
+                placeholder="Nome de Usuário" 
+                type="text"
+                icon={FiUser}
+                />
                 <Input 
                 placeholder="E-mail" 
                 type="text"
@@ -23,12 +29,12 @@ export function SignIn(){
                 icon={FiLock}
                 />
 
-                <Button title="Entrar" />
+                <Button title="Cadastrar" />
 
-                <a href="#">Criar Conta</a>
+                <a href="#">Fazer Login</a>
             </Form>
 
-            <Background />
+            
         </Container>
     )
 }
