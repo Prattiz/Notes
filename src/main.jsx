e3d2a807dde3a0a4583e09cd20365a2064acd45e
Thiago <prattithiago5@gmail.com>
2023-09-07 14:57:08 -0300

    CREATE auth.routes AND Fixing Bug

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
 import theme  from './styles/theme';
 
-import { New } from "./pages/New/index"
+import { Routes } from "./routes/index"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>  
     <GlobalStyles />
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )
--- src/pages/SignIn/style.js
+++ src/pages/SignIn/style.js
@@ -23,7 +23,7 @@ export const Form = styled.form`
 
     >h2{
         font-size: 24px;
-        margin-top: 48px 0;
+        margin: 48px 0;
         
     }
 
