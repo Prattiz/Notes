efe3f0696bf5331e20687406fd192bbce53bf9c1
Thiago <prattithiago5@gmail.com>
2023-09-14 21:24:09 -0300

    LOGOUT AND LOGIN COMPLETED

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -37,6 +37,28 @@ export function AuthProvider({ children }){
     setData({ })
    }
 
+   async function updateProfile({user}) {
+    try {
+      
+     
+  
+      await api.put("/users", user );
+      
+      
+      localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
+      
+      setData({ user, token: data.token });
+  
+      alert("Perfil atualizado");
+    } catch(error) {
+      if(error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possível atualizar o perfil.")
+      }
+    }
+  }
+
 
     useEffect(() => {
         const token = localStorage.getItem("@rocketnotes:token");
@@ -56,7 +78,7 @@ export function AuthProvider({ children }){
 
     return(
         <AuthContext.Provider 
-        value={{ signIn, user: data.user, signOut }}>
+        value={{ signIn, user: data.user, signOut, updateProfile}}>
             {children}
         </AuthContext.Provider>
     )
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,10 +1,24 @@
 import { Container, Form, Avatar} from "./styles";
+import { useState } from "react";
+import { useAuth } from "../../hooks/auth";
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Buttons";
 import { Link } from "react-router-dom";
 
 export function Profile(){
+    const { user, updateProfile } = useAuth();
+    const [ name, setName ] = useState(user.name);
+    const [ password, setPassword ] = useState();
+    const [ newPass, setNew ] = useState();
+    const [ email, setEmail ] = useState(user.email);
+    const [ avatar, setAvatar] = useState(null)
+
+    async function handleUpdate(){
+        const user = { name, email, password: newPass, oldPassword: password,}
+        await updateProfile({ user })
+    }
+    
     return(
         <Container>
             <header>
@@ -16,7 +30,7 @@ export function Profile(){
             <Form>
                 <Avatar>
                     <img 
-                    src="https://github.com/Prattiz.png" 
+                    src={avatar} 
                     alt="Foto do Usuário" 
                     />
 
@@ -33,24 +47,30 @@ export function Profile(){
                 placeholder="Nome de Usuário"
                 icon={FiUser}
                 type="text"
+                value={name}
+                onChange={ e => setName(e.target.value)}
                 />
                 <Input 
                 placeholder="E-mail"
                 icon={FiMail}
                 type="text"
+                value={email}
+                onChange={ e => setEmail(e.target.value)}
                 />
                 <Input 
                 placeholder="Senha Atual"
                 icon={FiLock}
                 type="password"
+                onChange={ e => setPassword(e.target.value)}
                 />
                 <Input 
                 placeholder="Nova Senha"
                 icon={FiLock}
                 type="password"
+                onChange={ e => setNew(e.target.value)}
                 />
 
-                <Button title="Salvar"/>
+                <Button title="Salvar" onClick={ handleUpdate }/>
 
 
             </Form>
