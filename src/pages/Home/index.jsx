4d1b08040640d6aab5df8e3b9e22645e589aa106
Thiago <prattithiago5@gmail.com>
2023-09-19 14:53:54 -0300

    ADD Tags at HOME

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,27 @@
 import { Conteiner, Brand, Search, Menu, Content, NewNote } from "./style";
 import { FiPlus, FiSearch } from "react-icons/fi";
+import { useState, useEffect } from "react";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
 import { ButtonText} from "../../components/ButtonT";
 import { Input } from "../../components/Input";
 import { Notes } from "../../components/Note";
+import { api } from "../../services/api";
 
 export function Home() {
+    
+    const [ tags, setTags ] = useState([]);
+
+    useEffect(() => {
+        async function fetchTags() {
+            const response = await api.get("/tags");
+            setTags(response.data)
+        }
+
+        fetchTags()
+    }, []);
+
+
     return(
         <Conteiner>
             <Brand>
@@ -16,10 +31,15 @@ export function Home() {
             <Header />
 
             <Menu>
-                
                 <li><ButtonText title="Ativo" /></li>
-                <li><ButtonText title="Teste"/></li>
-                <li><ButtonText title="Teste"/></li>
+
+                {
+                    tags && tags.map(tag => (
+                    <li key={String(tag.id)}><ButtonText title={tag.name} /></li>
+                    ))
+                }
+                
+
                 
                 
                 
